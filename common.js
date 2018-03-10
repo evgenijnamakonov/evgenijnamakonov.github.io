0b675411042d60e538e728a68c784a31e45377c2
evgenijnamakonov <evgenijnamakonov@gmail.com>
2018-03-10 18:48:42 +0300

    Сверстал страницу регистрации, настроил редирект

--- common.js
+++ common.js
@@ -1,5 +1,15 @@
-function helloWorld() {
+import MyMobileCrm from "./myCrm";
 
+function saveOrder() {
+
+    /**
+     * Например на форме заказа есть поля с кол-вом товара, адрессом доставки,
+     * Именем клиента и его телефоном
+     * Тогда по клику на кнопку "Отправить" вызовется метод saveOrder()
+     * Он в свою очередь соберет из текстовых полей введенные занчения и
+     * Положит их в объект data.
+     * @type {{amount, address, name, phone}}
+     */
     let data = {
         amount: document.getElementById('input-amount').value,
         address: document.getElementById('input-address').value,
@@ -7,7 +17,39 @@ function helloWorld() {
         phone: document.getElementById('input-phone').value,
     };
 
-    this.addOrder(data).then((response) => {
+    /**
+     * Затем создатся экземпляр класса MyMobileCrm
+     * и у этого экземпляра вызовется метод addOrder() в который и передадим data. И запись добавится в firebase
+     * @type {MyMobileCrm}
+     */
+    let CRM = new MyMobileCrm();
+
+    CRM.addOrder(data).then((response) => {
+        console.log(response)
+    }).catch((ExtendedError) => {
+        console.log(ExtendedError.message)
+    });
+}
+
+function saveUser() {
+
+    /**
+     * Метод создаст в базе запись о новом пользователе
+     * @type {{amount, address, name, phone}}
+     */
+    let data = {
+        amount: document.getElementById('input-amount').value,
+        address: document.getElementById('input-address').value,
+        name: document.getElementById('input-name').value,
+        phone: document.getElementById('input-phone').value,
+    };
+
+    /**
+     * @type {MyMobileCrm}
+     */
+    let CRM = new MyMobileCrm();
+
+    CRM.addUser(data).then((response) => {
         console.log(response)
     }).catch((ExtendedError) => {
         console.log(ExtendedError.message)
--- myCrm.js
+++ myCrm.js
@@ -1,51 +1,106 @@
-async function addOrder(data) {
+/**
+ * Класс myMobileCrm выполняет rest запросы к firebase
+ * Умеет создавать заказ, создавать нового пользователя сайта
+ */
+export default class MyMobileCrm {
 
-    let xhr = new XMLHttpRequest();
+    /**
+     * Метод асинхронный, возвращает Promise с информацией о выполненном запросе.
+     * Добавляет в базу запись о создании заказа
+     * Данные о заказе передаются в аргументе data
+     * data - это объект, например такого вида:
+     * {
+     *   date: 12.06.2018,
+     *   buyer: 'Amy',
+     *   sum: 1299,
+     *   phone: 79999999999,
+     *   product: 'Футболка'
+     * }
+     * может осдержать произвольные свойства и произвольное кол-во свойств
+     * @param data
+     * @returns {Promise<any>}
+     */
+    async addOrder(data) {
 
-    let request = new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
 
-        try {
-            xhr.open('POST', "https://my-mobile-crm-1331.firebaseio.com/apps/" + token + "/" + id + "/orders.json", false);
+        return new Promise((resolve, reject) => {
 
-            xhr.send(JSON.stringify({ ...data }));
+            try {
+                xhr.open('POST', "https://my-mobile-crm-1331.firebaseio.com/apps/" + token + "/" + id + "/orders.json", false);
 
-            switch ( xhr.status ) {
-                case 200:
-                    resolve(JSON.parse(xhr.response));
-                    return null;
-                case 400:
-                    reject(new Error('bad request'));
-                    return null;
-                case 401:
-                    reject(new Error('unauthorized request 401'));
-                    return null;
-                case 404:
-                    reject(new Error('Database was not found'));
-                    return null;
-                case 500:
-                    reject(new Error('The server returned an error. See the error message for further details'));
-                    return null;
-                case 503:
-                    reject(new Error('Service Unavailable'));
-                    return null;
-                case 412:
-                    reject(new Error('Precondition Failed'));
-                    return null;
+                xhr.send(JSON.stringify({ ...data }));
+
+                switch ( xhr.status ) {
+                    case 200:
+                        resolve(JSON.parse(xhr.response));
+                        return null;
+                    case 400:
+                        reject(new Error('bad request'));
+                        return null;
+                    case 401:
+                        reject(new Error('unauthorized request 401'));
+                        return null;
+                    case 404:
+                        reject(new Error('Database was not found'));
+                        return null;
+                    case 500:
+                        reject(new Error('The server returned an error. See the error message for further details'));
+                        return null;
+                    case 503:
+                        reject(new Error('Service Unavailable'));
+                        return null;
+                    case 412:
+                        reject(new Error('Precondition Failed'));
+                        return null;
+                }
             }
-        }
 
-        catch ( e ) {
-            throw Error('Unknown error')
-        }
-    });
+            catch ( e ) {
+                throw Error('Unknown error')
+            }
+        });
+    }
 
-    return request;
-}
+    async addUser(data) {
 
-async function getOrders() {
+        let xhr = new XMLHttpRequest();
 
-}
+        return new Promise((resolve, reject) => {
 
-function addUser() {
+            try {
+                xhr.open('POST', "https://my-mobile-crm-1331.firebaseio.com/apps/" + token + "/" + id + "/users.json", false);
 
+                xhr.send(JSON.stringify({ ...data }));
+
+                switch ( xhr.status ) {
+                    case 200:
+                        resolve(JSON.parse(xhr.response));
+                        return null;
+                    case 400:
+                        reject(new Error('bad request'));
+                        return null;
+                    case 401:
+                        reject(new Error('unauthorized request 401'));
+                        return null;
+                    case 404:
+                        reject(new Error('Database was not found'));
+                        return null;
+                    case 500:
+                        reject(new Error('The server returned an error. See the error message for further details'));
+                        return null;
+                    case 503:
+                        reject(new Error('Service Unavailable'));
+                        return null;
+                    case 412:
+                        reject(new Error('Precondition Failed'));
+                        return null;
+                }
+            }
+
+            catch ( e ) {
+                throw Error('Unknown error')
+            }
+        });
+    }
 }
